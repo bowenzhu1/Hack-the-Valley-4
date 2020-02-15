80b657b26f049470c536c6ff43239fbcf050a7a2
helpmynamewontfi <aydan.jiwani@gmail.com>
2020-02-15 10:06:16 -0500

    added database params

--- stitch/mongodb.js
+++ stitch/mongodb.js
@@ -4,10 +4,10 @@ import { app } from "./app";
 // TODO: Initialize a MongoDB Service Client
 const mongoClient = app.getServiceClient(
   RemoteMongoClient.factory,
-  "mongodb-atlas" // might need to change this if its named differently @aydan
+  "mongodb-atlas" // fixed the params in this file --Aydan
 );
 
 // TODO: Instantiate a collection handle for todo.items
-const items = mongodb("SOMETHING HERE").collection("SOMETHING ELSE")
+const items = mongodb("sleepwithhomies").collection("sleepusers")
 
 export { items };
