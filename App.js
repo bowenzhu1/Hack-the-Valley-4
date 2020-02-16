30b39c0b65971456e280e13a01d23b85973d7c84
Bill Li <bill.l1564262@gmail.com>
2020-02-15 23:55:25 -0500

    Complete CRUD calls

--- App.js
+++ App.js
@@ -27,7 +27,9 @@ import {
 
 import ReactNativeAN from 'react-native-alarm-notification';
 import { DeviceEventEmitter } from 'react-native';
-import { Stitch, AnonymousCredential, GoogleRedirectCredential } from "mongodb-stitch-react-native-sdk";
+import { Stitch, AnonymousCredential, UserPasswordCredential } from "mongodb-stitch-react-native-sdk";
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin'
+import { RemoteMongoClient, UserPasswordAuthProviderClient } from "mongodb-stitch-browser-sdk";
 
 const APP_ID = 'sleepwithhomies-vfaav';
 
@@ -62,6 +64,7 @@ const App: () => React$Node = () => {
   const [client, setClient] = useState(undefined);
   const [user, setUser] = useState(undefined);
   const [userId, setUserId] = useState(undefined);
+  const [mongoClient, setMongoClient] = useState(undefined);
 
   setAlarm = (time) => {
     //TODO sync alarm data with mongodb, this is only local side
@@ -86,7 +89,6 @@ const App: () => React$Node = () => {
   };
 
   deleteAlarm = (id) => {
-    //TODO sync with user's group
     ReactNativeAN.deleteAlarm(id);
   }
 
@@ -95,8 +97,7 @@ const App: () => React$Node = () => {
   }
 
   getAlarms = () =>{
-    // TODO get the alarms from the user's group(s) from mongodb and sync them locally
-    return ReactNativeAN.getScheduledAlarms(); // TODO also remove this garbage
+    return ReactNativeAN.getScheduledAlarms();
   }
 
   removeNotifications = () => {
@@ -104,49 +105,266 @@ const App: () => React$Node = () => {
   }
 
   loadClient = () => {
-    Stitch.initializeDefaultAppClient(APP_ID).then(client =>{
-      setClient(client);
-      client.auth.loginWithCredential(new AnonymousCredential()) // TODO oauth
-        .then(user => {
-          console.log('successfully logged in anonymously as ${user.id}');
-          setUserId(user.id);
-          setUserId(client.auth.user.id);
-        })
-        .catch(err => {
-          console.log('failed to log in: ${err}');
-          setUserId(undefined);
-        });
-     });
+    return new Promise ((res, rej) => {
+      Stitch.initializeDefaultAppClient(APP_ID).then(client =>{
+        setClient(client);
+        client.auth.loginWithCredential(new AnonymousCredential()) // TODO oauth
+          .then(user => {
+            setUser(user);
+            console.log(`successfully logged in anonymously as ${user.id}`);
+            const mongoClient = client.getServiceClient(
+              RemoteMongoClient.factory,
+              "mongodb-atlas"
+            );
+            setMongoClient(mongoClient);
+            res('success');
+          })
+          .catch(err => {
+            console.log(`failed to log in: ${err}`);
+            setUserId(undefined);
+            rej(err);
+          });
+       });
+    });
    }
 
+   loginWithEmail = async (email, password) => {
+      const credential = new UserPasswordCredential(email, password);
+      client.auth.loginWithCredential(credential)
+        .then(user => {
+          console.log(`successfully logged in with id: ${user.id}`)
+          console.log(client.auth.user.profile);
+          setUser(user);
+        })
+        .catch(err => console.error(`login failed with error: ${err}`))
+   }
 
-     //  const client = Stitch.hasAppClient(APP_ID)
-     //    ? Stitch.getAppClient(APP_ID)
-     //    : Stitch.initializeAppClient(APP_ID);
-     //
-     //  if (client.auth.hasRedirectResult()) {
-     //    client.auth.handleRedirectResult().catch(console.error);
-     //    console.log("Processed redirect result.")
-     //  }
-     //
-     // if (client.auth.isLoggedIn) {
-     //   // The user is logged in. Add their user object to component state.
-     //   setUser = client.auth.user;
-     // } else {
-     //   // The user has not yet authenticated. Begin the Google login flow.
-     //   const credential = new GoogleRedirectCredential();
-     //   client.auth.loginWithRedirect(credential);
-     // }
-    //}
+   signupWithEmail = (name, email, password) => {
+     client.auth.getProviderClient(UserPasswordAuthProviderClient.factory, "userpass")
+        .registerWithEmail(email, password)
+        .then(() => console.log("Successfully signed up"))
+        .catch(err => console.error("Error registering new user:", err));
+      const sleepUsers = mongoClient.db("sleepwithhomies").collection("sleepusers");
+      const newUser = {
+        'name': name,
+        'id': client.auth.user.id,
+      };
+      sleepUsers.insertOne(newUser)
+        .then(result => console.log(`success: inserted at ${result.insertedId}`))
+        .catch(err => console.error(`Failed to insert item: ${err}`));
+   }
 
-
-
-  joinGroup = (id) => {
+  joinGroup = async (code) => {
     //TODO add user to group and sync with db
+    if(mongoClient){
+      const groups = mongoClient.db("sleepwithhomies").collection("groups");
+      const leaderboards = mongoClient.db("sleepwithhomies").collection("leaderboards");
+      const query = {"code" : code};
+      const queryBoard = {"groupCode" : code};
+      groups.findOne(query)
+        .then(doc => {
+          if(doc){
+            const userId = client.auth.user.id;
+
+            leaderboards.findOne(queryBoard)
+              .then(doc2 => {
+                console.log(doc2);
+                const updateBoard = {
+                  "$set": {
+                    "scores": {...doc2.scores, [userId]: 0},
+                  }
+                }
+                const queryBoard = {"groupCode" : code};
+                leaderboards.updateOne(queryBoard, updateBoard, {})
+                  .then(result => {
+                    const { matchedCount, modifiedCount } = result;
+                    if(matchedCount && modifiedCount) {
+                      console.log('successfully update leaderboard');
+                    }
+                  })
+                  .catch(err => console.error(`failed to update leaderboard: ${err}`));
+              })
+              .catch(err => console.error(`failed to find leaderboard: ${err}`));
+
+            // update group
+            const usersArr = doc.users.concat([userId]);
+            const update = {
+              "$set": {
+                "users": usersArr,
+              }
+            };
+            groups.updateOne(query, update, {})
+              .then(result => {
+                const { matchedCount, modifiedCount } = result;
+                if(matchedCount && modifiedCount) {
+                  console.log('successfully update groups');
+                }
+              })
+              .catch(err => console.error(`failed to update: ${err}`));
+
+          }else{
+            console.log(`couldn't find group, creating one now:`);
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+            let result = '' //not efficient but whatever
+            for(let i = 0; i < 6; i++){
+              result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            const newGroup = {
+              "name": "New Group",
+              "code": result,
+              "users": [client.auth.user.id],
+            };
+
+            const newLeaderboard = {
+              "groupCode": result,
+              "scores": {[client.auth.user.id]: 0},
+            };
+
+            groups.insertOne(newGroup)
+              .then(result => console.log(`success: inserted at ${result.insertedId}`))
+              .catch(err => console.error(`Failed to insert item: ${err}`));
+
+            leaderboards.insertOne(newLeaderboard)
+              .then(result => console.log(`success: inserted at ${result.insertedId}`))
+              .catch(err => console.error(`Failed to insert item: ${err}`));
+
+          }
+        })
+        .catch(err => {
+          console.log(`failed: ${err}`);
+        });
+      }
   }
 
-  leaveGroup = () => {
-    //TODO remove user from group and sync with db
+  getGroups = (id=client.auth.user.id) => {
+    return new Promise ((res, rej) => {
+      if(mongoClient){
+        const groups = mongoClient.db("sleepwithhomies").collection("groups");
+        const query = {"users" : id};
+        groups.find(query).toArray()
+          .then(docs => {
+            console.log(`groups got successfully: ${docs}`)
+            const groupArr = [];
+            docs.forEach(element => groupArr.push(element));
+            res(groupArr);
+          })
+          .catch(err => {
+            rej(Error(`failure getting groups: ${err}`));
+          })
+      }else{
+        rej(Error("No mongoClient?"));
+      }
+    });
+  }
+
+  leaveGroup = (code) => {
+    if(mongoClient){
+      const groups = mongoClient.db("sleepwithhomies").collection("groups");
+      const query = {"code" : code};
+      groups.findOne(query)
+        .then(doc => {
+          if(doc){
+            let users = doc.users
+            users.splice(users.findIndex(element => element == client.auth.user.id), 1)
+            const update = {
+              "$set": {
+                "users": users,
+              }
+            };
+            groups.updateOne(query, update, {})
+              .then(result => {
+                const { matchedCount, modifiedCount } = result;
+                if(matchedCount && modifiedCount) {
+                  console.log('success');
+                }
+              })
+              .catch(err => console.error(`failed to update: ${err}`));
+          }else{
+            console.log(`couldn't find group`);
+          }
+        })
+        .catch(err => {
+          console.log(`failed: ${err}`);
+        });
+      }
+  }
+
+  updateLeaderboard = (code, update={}) => {
+    if(mongoClient){
+      const leaderboards = mongoClient.db("sleepwithhomies").collection("leaderboards");
+      const query = {"groupCode" : code};
+      leaderboards.findOne(query)
+        .then(doc => {
+          if(doc){
+            let scores = doc.scores;
+            for(let [key, value] of Object.entries(update)){
+                scores[key] += value;
+            }
+            const updateBoard = {...doc, 'scores': scores}
+            leaderboards.updateOne(query, updateBoard, {})
+              .then(result => {
+                const { matchedCount, modifiedCount } = result;
+                if(matchedCount && modifiedCount) {
+                  console.log('success updating leaderboard');
+                }
+              })
+              .catch(err => console.error(`failed to update: ${err}`));
+          }else{
+            console.log(`couldn't find leaderboard or no update needed`);
+          }
+        })
+        .catch(err => {
+          console.log(`failed: ${err}`);
+        });
+      }
+  }
+
+  getUsers = (code) => {
+    return new Promise((res, rej) => {
+      if(mongoClient){
+        const groups = mongoClient.db("sleepwithhomies").collection("groups");
+        const leaderboards = mongoClient.db("sleepwithhomies").collection("leaderboards");
+        const sleepUsers = mongoClient.db("sleepwithhomies").collection("sleepusers");
+        const query = {'code': code};
+        const queryBoard = {'groupCode': code};
+
+        leaderboards.findOne(queryBoard).then(doc => {
+            if(doc){
+              let ids = [];
+              let boardUsers = [];
+              for(let [key, value] of Object.entries(doc.scores)){
+                  ids.push(key);
+              }
+              console.log(`ids:${ids}`);
+              const queryUser = {'id': { '$in': ids}};
+              sleepUsers.find(queryUser).toArray().then(docs => {
+                docs.forEach(element => boardUsers.push(element));
+                console.log(`success creating boardUsers: ${boardUsers}`)
+                return boardUsers;
+              }).then(boardUsers => {
+                console.log(`boardUsers: ${boardUsers}`)
+                groups.findOne(query).then(doc => {
+                  const groupUsers = [];
+                  doc.users.forEach(user => {
+                    const index = boardUsers.findIndex(boardUser => boardUser.id == user);
+                    if(index != -1){
+                      groupUsers.push(boardUsers[index]);
+                    }
+                  })
+
+                res({
+                    group: groupUsers,
+                    leaderboard: boardUsers,
+                  });
+                }).catch(err => console.log(`failed finding group: ${err}`));
+              }).catch(err => console.log(`error getting users: ${err}`));
+            }
+          }).catch(err => console.log(`failed finding users from board: ${key}`));
+      }else{
+        rej("No mongoClient?");
+      }
+    });
   }
 
   useEffect(() => {
@@ -156,15 +374,19 @@ const App: () => React$Node = () => {
         console.log(obj);
         stopAlarm();
       });
-      ReactNativeAN.scheduleAlarm(alarmNotifData);
-      //ReactNativeAN.sendNotification(alarmNotifData);
 
       DeviceEventEmitter.addListener('OnNotificationOpened', async function(e) {
         const obj = JSON.parse(e);
         console.log(obj);
       });
 
-      loadClient();
+      loadClient().then(() => {
+        console.log('Client loaded successfully');
+        //TODO
+      })
+      .catch(err => {
+        console.log(`MongoDB client failed to load: ${err}`);
+      });
 
       return function cleanup() {
         DeviceEventEmitter.removeListener('OnNotificationDismissed');
