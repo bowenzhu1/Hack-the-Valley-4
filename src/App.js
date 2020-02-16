8c1728fe90971f0e6b82d955eeb1676540c0aeab
Bill Li <bill.l1564262@gmail.com>
2020-02-16 03:26:37 -0500

    Finish game 1

--- src/App.js
+++ src/App.js
@@ -7,110 +7,103 @@ import triangle from "./assets/triangle.svg";
 import whitebox from "./assets/whitebox.jpg";
 import circle from "./assets/full-moon.svg";
 
-function Card({parentCallback, image, i}) {
-  const [flipped, setFlipped] = useState(false)
-  const [matched, setMatched] = useState(false)
-
-  function sendData() {
-    setFlipped(!flipped)
-    console.log("flip")
-    parentCallback(i, flipped, matched)
-  }
-
-  function displayCard() {
-    if (matched) {
-      return whitebox
-    } else if (flipped) {
-      return image
-    }
-    return question
-  }
+const Card = ({key, index, img, flipped, matched}) => {
 
   return (
-    <div className="Card" onClick={sendData}>
-      <img style={{width: 200, height: 200}}
-        src={displayCard()} />
+    <div className="Card">
+      <img
+        style={{width: 150, height: 150, padding: 20}}
+        src={matched ? whitebox : flipped ? img : question}
+      />
     </div>
   )
 }
 
-function App() {
+const App = () => {
   const [cardlist, setCardlist] = useState([])
-  useEffect(() => {
-    const images = [star, diamond, triangle, circle]
-    const doubleImages = images.concat(images)
-    const range = doubleImages.length
-    let deck = []
-    for (let i = 0; i < range; i++) {
-      let randomIndex = Math.floor(Math.random() * doubleImages.length)
-      deck.push({image: doubleImages[randomIndex],
-                 flipped: false,
-                 matched: false
+  const [time, setTime] = useState(new Date())
+
+  const handleClick = (index) => () => {
+    let temp = cardlist
+    temp[index].flipped = true;
+
+    let numOfFlipped = 0
+    let flipped = []
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i].flipped) {
+        numOfFlipped++
+        flipped.push(i)
+      }
+    }
+    if (flipped.length === 2 && temp[flipped[0]].img === temp[flipped[1]].img) {
+      setTimeout(() => {
+        temp[flipped[0]].matched = true;
+        temp[flipped[1]].matched = true;
+        temp[flipped[0]].flipped = false;
+        temp[flipped[1]].flipped = false;
+        setCardlist([...temp])
+      }, 400)
+    } else if (numOfFlipped == 2) {
+      setTimeout(() => {
+        for (let i = 0; i < temp.length; i++) temp[i].flipped = false
+        setCardlist([...temp])
+      }, 400)
+    }
+    setCardlist([...temp])
+  }
+
+  const renderCards = () => {
+    let finished = true
+    for (let card of cardlist) {
+      if (!card.matched) {
+        finished = false
+        break;
+      }
+    }
+
+    if (!finished) {
+      return cardlist.map((card, index) => {
+        return (
+            <div onClick={handleClick(index)} key={index}>
+              <Card
+                index={index}
+                img={card.img}
+                flipped={card.flipped}
+                matched={card.matched}
+              />
+            </div>
+        )
       })
-      doubleImages.splice(randomIndex, 1)
+    } else {
+      const cur = new Date()
+      const score = String(Math.round(1 / (cur - time) * 1000000))
+      return (
+          <p>{score}</p>
+      )
+    }
+  }
+
+  useEffect(() => {
+    let images = [star, diamond, triangle, circle]
+    let deck = []
+    images = images.concat(images)
+    const length = images.length
+
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * images.length)
+      deck.push({
+        img: images[index],
+        flipped: false,
+        matched: false
+      })
+      images.splice(index, 1)
     }
     setCardlist(deck)
   },[])
 
-  function displayCard(card) {
-    if (card.matched) {
-      return whitebox
-    } else if (card.flipped) {
-      console.log("displayed")
-      return card.image
-    } else {
-    console.log("Displayed question")
-    return question
-    }
-  }
-
-  function makeDeck() {
-    let deck = []
-    console.log("we are making a deck")
-    for (let i = 0; i < cardlist.length; i++) {
-      deck.push(<Card i={i} image={displayCard(cardlist[i])} parentCallback={callbackFunction} />)
-    }
-    return deck
-  }
-
-  function callbackFunction(i, flipped, matched) {
-    let temp = cardlist
-    let flippedCount = 0
-    let flippedCards = []
-    temp[i].flipped = !flipped
-    temp[i].matched = matched
-    console.log(temp)
-    for (let x = 0; x < temp.length; x++) {
-      if (temp[x].flipped) {
-        flippedCards.push(x)
-        flippedCount += 1
-      }
-    }
-    if (flippedCount >= 2) {
-      console.log("we made it")
-      console.log(flippedCards)
-      if (temp[flippedCards[0]].image == temp[flippedCards[1]].image) {
-        console.log(temp)
-        temp[flippedCards[0]].matched = true
-        temp[flippedCards[1]].matched = true
-        temp[flippedCards[0]].flipped = false
-        temp[flippedCards[1]].flipped = false
-        console.log("match found")
-      } else {
-      temp[flippedCards[0]].flipped = false
-      temp[flippedCards[1]].flipped = false
-      flippedCards = []
-      flippedCount = 0
-      console.log("shin ryujin")
-      }
-    }
-    setCardlist(temp)
-    console.log(cardlist)
-  }
-
   return (
     <div className="Board">
-      {makeDeck()}
+      {renderCards()}
     </div>
   );
 }
